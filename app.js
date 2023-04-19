4a96e9bb8eba7c37281ad9b7e623cb7e226a0255
Rabia Tanweer <rabiatanveer27@gmail.com>
2023-04-19 16:11:11 +0500

    final

--- app.js
+++ app.js
@@ -47,20 +47,48 @@ function searchRecipe() {
   <div class="card" onclick="display(${index})">
   <h1 class="title">${value.title}</h1>
   <img src="${value.image}" alt="img">
+  <h4 class="ingredient_title"> <u>Ingredients: </u></h4>
   <ul class="ingredients">${value.extendedIngredients.map (X=> `<li>${X.name}</li>`
-  )}</ul>
+  ).join(" ")}</ul>
   `).join('');
   recipeContainer.innerHTML = foodcardshtml;
 //   If search box==="". // Is main search box ki value honi Chahiye
 // Document.getElementById("product-list")= myRecipeData. // Is main map kiye hue products
 }
  function display(y) {
+  const searchBar = searchTerm.value.toLowerCase();
+
+  const filteredProducts = completedata.filter((product) => {
+    const recipeName = product.title.toLowerCase();
+    return recipeName.includes(searchBar) 
+  });
+  
 
 document.getElementsByClassName("popup")[0].classList.add("active");
 Array.from(document.getElementsByClassName("card")).forEach((el) =>{
   el.classList.add("hide")
  })
+if (searchBar==="") {
+  
+  const rabia=completedata[y].extendedIngredients.map(x=>{
+   return ` 
+   <p>${x.original}</p>
+   `
+  }).join(" ")
+  document.getElementById("recipeIngredients").innerHTML = rabia
 document.getElementById("myRecipe").innerHTML= completedata[y].instructions;
+}
+else{
+  const rabia=filteredProducts[y].extendedIngredients.map(x=>{
+    return ` 
+    <p>${x.original}</p>
+    `
+   }).join(" ")
+   document.getElementById("recipeIngredients").innerHTML = rabia
+  document.getElementById("myRecipe").innerHTML= filteredProducts[y].instructions
+}
+
+// document.getElementById("myRecipe").innerHTML= completedata[y].instructions;
 
 document.getElementById("dismiss-popup-btn").addEventListener("click", function(){
   document.getElementsByClassName("popup")[0].classList.remove("active");
