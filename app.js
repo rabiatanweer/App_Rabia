1819b2cc66bbde321f8671f613f9758f2b6cc991
Rabia Tanweer <rabiatanveer27@gmail.com>
2023-04-19 23:27:37 +0500

    final_app

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 
-// main cards with pic and title
-// let completedata = null;
-// const endpoint = 'https://api.spoonacular.com/recipes/random?apiKey=e4df68706b174577ae41be782fd11179&number=10';
+
+let completedata = null;
+const endpoint = 'https://api.spoonacular.com/recipes/random?apiKey=e4df68706b174577ae41be782fd11179&number=10';
  
-// // Get recipe data from Spoonacular API
-// fetch(endpoint)
-//   .then(response => response.json())
-//   .then(data => {
-//    const mydata = localStorage.setItem("data_recipe", JSON.stringify(data.recipes))})
+// Get recipe data from Spoonacular API
+fetch(endpoint)
+  .then(response => response.json())
+  .then(data => {
+   const mydata = localStorage.setItem("data_recipe", JSON.stringify(data.recipes))})
 let myData=localStorage.getItem("data_recipe")
 
 
     completedata =JSON.parse(myData);
     console.log(completedata);
 
-    // Generate recipe cards
+    // Generate recipe cards after fetching data from API
     
     let data1=completedata.map((value,index) => {
       return  `
@@ -22,8 +22,8 @@ let myData=localStorage.getItem("data_recipe")
         <h1 class="title">${value.title}</h1>
         <img src="${value.image}" alt="img">
         <h4 class="ingredient_title"> <u>Ingredients: </u></h4>
-        <p class="ingredients">${value.extendedIngredients.map (X=> X.name
-        )}</p>
+        <ul class="ingredients">${value.extendedIngredients.map (ingredient=> `<li>${ingredient.name}</li>`
+  ).join(" ")}</ul>
      
         
        
@@ -31,35 +31,35 @@ let myData=localStorage.getItem("data_recipe")
     });
     document.getElementById("cards").innerHTML = data1;
   
-  // search Recipie
+  // searching bar for  Recipe
 let searchTerm = document.getElementById("searchItem")
 
 searchTerm.addEventListener('keyup', searchRecipe);
 function searchRecipe() {
   const searchBar = searchTerm.value.toLowerCase();
 
-  const filteredProducts = completedata.filter((product) => {
-    const recipeName = product.title.toLowerCase();
+  const filteredRecipeData = completedata.filter((recipe) => {
+    const recipeName = recipe.title.toLowerCase();
     return recipeName.includes(searchBar) 
   });
   const recipeContainer = document.querySelector('#cards');
-  const foodcardshtml = filteredProducts.map((value, index) => `
+  const foodcardshtml = filteredRecipeData.map((value, index) => `
   <div class="card" onclick="display(${index})">
   <h1 class="title">${value.title}</h1>
   <img src="${value.image}" alt="img">
   <h4 class="ingredient_title"> <u>Ingredients: </u></h4>
-  <ul class="ingredients">${value.extendedIngredients.map (X=> `<li>${X.name}</li>`
+  <ul class="ingredients">${value.extendedIngredients.map (ingredient=> `<li>${ingredient.name}</li>`
   ).join(" ")}</ul>
   `).join('');
   recipeContainer.innerHTML = foodcardshtml;
-//   If search box==="". // Is main search box ki value honi Chahiye
-// Document.getElementById("product-list")= myRecipeData. // Is main map kiye hue products
+
 }
+// showing recipe and quantity of ingredients
  function display(y) {
   const searchBar = searchTerm.value.toLowerCase();
 
-  const filteredProducts = completedata.filter((product) => {
-    const recipeName = product.title.toLowerCase();
+  const filteredRecipeData = completedata.filter((recipe) => {
+    const recipeName = recipe.title.toLowerCase();
     return recipeName.includes(searchBar) 
   });
   
@@ -70,25 +70,23 @@ Array.from(document.getElementsByClassName("card")).forEach((el) =>{
  })
 if (searchBar==="") {
   
-  const rabia=completedata[y].extendedIngredients.map(x=>{
+  const ingredientArray=completedata[y].extendedIngredients.map(IngredientQuantity=>{
    return ` 
-   <p>${x.original}</p>
+   <p>${IngredientQuantity.original}</p>
    `
   }).join(" ")
-  document.getElementById("recipeIngredients").innerHTML = rabia
+  document.getElementById("recipeIngredients").innerHTML = ingredientArray
 document.getElementById("myRecipe").innerHTML= completedata[y].instructions;
 }
 else{
-  const rabia=filteredProducts[y].extendedIngredients.map(x=>{
+  const ingredientArray=filteredRecipeData[y].extendedIngredients.map(IngredientQuantity=>{
     return ` 
-    <p>${x.original}</p>
+    <p>${IngredientQuantity.original}</p>
     `
    }).join(" ")
-   document.getElementById("recipeIngredients").innerHTML = rabia
-  document.getElementById("myRecipe").innerHTML= filteredProducts[y].instructions
+   document.getElementById("recipeIngredients").innerHTML = ingredientArray
+  document.getElementById("myRecipe").innerHTML= filteredRecipeData[y].instructions
 }
-
-// document.getElementById("myRecipe").innerHTML= completedata[y].instructions;
 
 document.getElementById("dismiss-popup-btn").addEventListener("click", function(){
   document.getElementsByClassName("popup")[0].classList.remove("active");
